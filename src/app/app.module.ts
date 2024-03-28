ba3bb1154f7cd01af717dfb0ebe07a91fedb1783
Luis <eternal_pitta0b@icloud.com>
2024-03-27 21:05:34 -0500

    create modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogEditComponent } from './blogs/blog-detail/blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/blogs/blog.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { BlogsComponent } from "./blogs.component";
+import { RouterModule } from "@angular/router";
+import { BlogsRoutingModule } from "./blog-routing.module";
+import { CommonModule } from "@angular/common";
+import { BlogListComponent } from "./blog-list/blog-list.component";
+import { HeaderComponent } from "../header/header.component";
+import { SharedModule } from "../shared/shared.module";
+import { BlogDetailComponent } from "./blog-detail/blog-detail.component";
+import { BlogEditComponent } from "./blog-detail/blog-edit/blog-edit.component";
+
+@NgModule({
+    declarations: [
+        BlogsComponent,
+        BlogListComponent,
+        BlogDetailComponent,
+        BlogEditComponent
+    ],
+    imports: [
+        RouterModule,
+        BlogsRoutingModule,
+        CommonModule,
+        SharedModule
+    ]
+})
+export class BlogModule{}
