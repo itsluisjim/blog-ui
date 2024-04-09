80d5bba7a3a99f2787773f972465f443630498aa
Luis <eternal_pitta0b@icloud.com>
2024-04-08 21:55:05 -0500

    Implement remaining API calls

--- src/app/blogs/blog.service.ts
+++ src/app/blogs/blog.service.ts
@@ -4,41 +4,60 @@ import { Observable } from 'rxjs';
 import { Blog } from './blog-list/blog.model';
 import { environment } from 'src/env/environment';
 
+export interface ResponseData {
+  message: string,
+  post: Blog
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
+  private url = `${environment.BASE_URL}/${environment.API_VERSION}`;
+
   constructor(private http: HttpClient) { }
+
+  // Get all Blogs
+  getAllBlogs(): Observable<Blog[]> {
+    return this.http.get<Blog[]>(`${this.url}/posts/`);
+  };
 
   // Get blog details by ID
   getBlogById(blogId: string): Observable<Blog> {
-    console.log("Inside getBlogById()")
-    return this.http.get<Blog>(`${environment.BASE_URL}${environment.API_VERSION}posts/${blogId}`);
+    return this.http.get<Blog>(`${this.url}/posts/${blogId}`);
   }
 
   // Create a new blog
-//   createBlog(blog: Blog): Observable<Blog> {
-//     return this.http.post<Blog>('/api/blogs', blog);
-//   }
+  createBlog(authorId: string, title: string, content: string, published: boolean): Observable<ResponseData> {
+    return this.http.post<ResponseData>(`${this.url}/posts/create`, {
+      authorId,
+      title,
+      content,
+      published
+    })
+  }
 
   // Update an existing blog
-//   updateBlog(blogId: string, updatedBlog: Blog): Observable<Blog> {
-//     return this.http.put<Blog>(`/api/blogs/${blogId}`, updatedBlog);
-//   }
+  updateBlog(url_blog_id: string, authorId: string, title: string, content: string, published: boolean): Observable<ResponseData> {
+    return this.http.put<ResponseData>(`${this.url}/posts/${url_blog_id}/update`, {
+      authorId,
+      title,
+      content,
+      published
+    });
+  }
 
   // Delete a blog
-//   deleteBlog(blogId: string): Observable<void> {
-//     return this.http.delete<void>(`/api/blogs/${blogId}`);
-//   }
-
-  // Get blog author ID by blog ID
-  static getBlogAuthorId(blogId: string): string {
-    // This is a placeholder method.
-    // You may implement logic to fetch blog author ID from the server.
-    // For demonstration purposes, assume the author ID is stored along with the blog.
-    // You may also cache blog author IDs to avoid repeated API calls.
-    // Replace this with your actual implementation.
-    return 'author123'; // Return a hardcoded author ID for demonstration
+  deleteBlog(postId: string): Observable<any> {
+    return this.http
+    .delete(
+      `${this.url}/posts/${postId}/delete`,
+      {
+        body: {
+          postId: postId,
+        },
+      }
+    );
   }
 }
