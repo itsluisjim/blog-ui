81d39af7954a6f8e7a1260472f21ecedbdf71375
Luis <eternal_pitta0b@icloud.com>
2024-04-13 15:21:03 -0500

    create component no-blogs
    this component is used when no blogs are found

---
+++ src/app/shared/no-blogs/no-blogs.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-no-blogs',
+  templateUrl: './no-blogs.component.html',
+  styleUrls: ['./no-blogs.component.css']
+})
+export class NoBlogsComponent {
+  @Input() message1: string;
+  @Input() message2: string;
+}
