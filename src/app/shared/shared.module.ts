9096641b03b5d2e453b9615757983eb0381882c2
Luis <eternal_pitta0b@icloud.com>
2024-04-13 15:23:17 -0500

    add NoBlogsComponent to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { NoBlogsComponent } from './no-blogs/no-blogs.component';
 
 @NgModule({
   declarations: [
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent, NoBlogsComponent
   ],
   imports: [CommonModule],
   exports: [
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    NoBlogsComponent
   ],
 })
 export class SharedModule {}
