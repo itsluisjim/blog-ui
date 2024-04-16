f32b1239b06b76258ed7e54c255825b730509aa0
Luis <eternal_pitta0b@icloud.com>
2024-04-15 21:03:49 -0500

    store user in variable
    user is necessary to get admin property to conditionally render manage authors and blogs page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { faSignOut } from '@fortawesome/free-solid-svg-icons'
+import { User } from '../auth/user.model';
 
 @Component({
   selector: 'app-header',
@@ -14,6 +15,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private userSubscription: Subscription;
   isAuthenticated = false;
+  user: User;
   
 
   constructor(private authService: AuthService) {}
@@ -21,6 +23,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
       this.userSubscription = this.authService.user.subscribe(user => {
        this.isAuthenticated = !!user;
+
+       if(this.isAuthenticated){
+        this.user = user;
+       }
       })
   }
 
